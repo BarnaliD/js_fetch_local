b69de44c0a20bde5be037a2669b80db8e3418d3e
BarnaliD <115993238+BarnaliD@users.noreply.github.com>
2023-04-18 13:31:57 +0200

    final Push

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* different ways of reading data*/
 // promise chaining
 function realFilePromise() {
-    fetch('customers.json')
+    fetch('customer.json')
     .then(response =>{
         if(!response.ok){
             throw new Error('Fetch error:' + response.status);
@@ -16,6 +16,24 @@ function realFilePromise() {
     })
     
 }
+//Async Function
+async function readFileAsync(){
+
+    let data = await fetch ('customers.json');
+    let customers = await data.json();
+    generateHTML(customers);
+
+}
+// Async function with error handeling
+async function readFileAsync2(){
+    try{
+        let data = await fetch ('customers.json');
+        let customers = await data.json();
+        generateHTML(customers);
+    }catch{
+        console.log('Error,could not read from json file');
+    }
+}
 
 //render HTML
 function generateHTML(customers){ 
@@ -24,6 +42,7 @@ function generateHTML(customers){
     html += ` 
     <h3>${customer.first_name} ${customer.last_name}</h3>
     <p>${customer.first_name} works at  ${customer.company_name}</p>
+    <p>${customer.first_name} is a ${customer.gender} and Email ID is ${customer.email}</p>
     `;
   }
     html += '<hr>'
@@ -32,4 +51,6 @@ function generateHTML(customers){
     document.querySelector('body').append(customerDiv)
  }
  realFilePromise();
+ readFileAsync();
+ readFileAsync2();
  
